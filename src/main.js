cc562823f7ea366772fc0dfc84f8688602cc303d
Shivam Mishra <scm.mymail@gmail.com>
2019-06-24 20:05:51 +0530

    feat: added vue-router

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import router from './router';
+
 import FeatherIcon from '@/components/Statics/FeatherIcon';
 import Logo from '@/components/Statics/Logo';
 
+// Component registration
 Vue.component('FeatherIcon', FeatherIcon);
 Vue.component('Logo', Logo);
+
+// Plugins
 Vue.use(require('vue-moment'));
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount('#app');
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Messages from '@/views/Messages';
+
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    {
+      path: '/message',
+      name: 'messages-home',
+      component: Messages
+    },
+    {
+      path: '/message/:id',
+      name: 'message',
+      component: Messages
+    }
+  ]
+});
+
+export default router;
